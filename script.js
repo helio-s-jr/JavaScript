5ed655cb780534bd65756cd751992b49e6bab654
Helio-Junior <heliojr.zpt@gmail.com>
2022-06-18 13:49:14 -0300

    Primeiro exercício de JavaScript

---
+++ script.js
@@ -0,0 +1,15 @@
+let nome = "Helio Junior"
+console.log(nome)
+
+let numero = 23
+console.log(numero)
+
+let ano = 2022
+console.log(ano - numero)
+
+console.log("Estou mexendo no console.log")
+
+let quartaFeira = false
+console.log(quartaFeira)
+
+console.log(typeof quartaFeira)
